ac244626761bed9610691b9c4c21bed46aa1a99c
Harsh Sidhpuria <harshsidhpuria@gmail.com>
2024-07-24 21:47:56 +0530

    Learned about Router.

--- Text-Converter/src/App.jsx
+++ Text-Converter/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Textarea from "./components/Textarea";
+import About from "./components/About";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [theme, setTheme] = useState();
+
   return (
     <>
       <Navbar Title="Text Converter" />
-      <Textarea />
+      <Routes>
+        <Route path="/textarea" element={<Textarea />} />
+
+        <Route path="/About" element={<About />} />
+      </Routes>
     </>
   );
 }
---
+++ Text-Converter/src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>This is a About Page.</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi
+        mollitia similique in error ut non enim laborum odit, dolorum eaque
+        earum eum? Repellat dolor accusantium quae exercitationem, iste
+        voluptatum?
+      </p>
+    </div>
+  );
+}
+
+export default About;
